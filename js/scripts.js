6255cb7cbd798ffb9651d6ef3c376831c711f1be
Henry Nguyen <belugas2005@gmail.com>
2023-09-11 16:05:09 -0500

    Added output with pokemon name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-pokemonList = 
-[{name:'bulbasaur', 
-height: .7, 
-type:[grass, posion]},
+let pokemonList = [
+  {name:'bulbasaur', 
+  height: .7, 
+  type:['grass', 'posion']},
 
-{name:'charmeleon',
-height:.6,
-type:[fire]},
+  {name:'charmeleon',
+  height:.6,
+  type:['fire']},
 
-{name:'squirtle',
-height:.5,
-type: [water],
-},]
+  {name:'squirtle',
+  height:.5,
+  type: ['water']},
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemon = pokemonList[i];
+  let label = '';
+  
+  if (pokemon.height > .6) {
+    label = ' - WOW! That\'s a big pokemon!';
+  };
+  
+  document.write(`${pokemon.name} (${pokemon.height}m) ${label}<br>`);
+};
