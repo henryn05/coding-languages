45f11d9d906bc283b0a9abd3648f22813612f6d4
Henry Nguyen <belugas2005@gmail.com>
2023-09-16 13:25:43 -0500

    Restricted 'add' function to only objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
-let pokemonRepository = (function() {
-  let pokemonList = [ 
-    {name: 'Bulbasaur', height: .7, type: ['Grass', 'Poison']},
-    {name: 'Charmander', height: .6, type: ['Fire']},
-    {name: 'Squirtle', height: .5, type: ['Water']},
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
+    { name: "Charmander", height: 0.6, type: ["Fire"] },
+    { name: "Squirtle", height: 0.5, type: ["Water"] },
+  ];
 
-return {
-  getAll: function() {
-    return pokemonList;
-  },
-  add: function(pokemon) {
-    pokemonList.push(pokemon);
-  },
-}
+  return {
+    getAll: function () {
+      return pokemonList;
+    },
+    add: function (pokemon) {
+      if (typeof pokemon === object) {
+        pokemonList.push(pokemon);
+      } else {
+        console.error("Error: Invalid data type. Only objects allowed");
+      }
+    },
+  };
 })();
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-    document.write(
-      'Name: ' + pokemon.name +
-      ' | Height: ' + pokemon.height + 'm<br>'
-      );
+  document.write(
+    "Name: " + pokemon.name + " | Height: " + pokemon.height + "m<br>"
+  );
 });
