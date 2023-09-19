82fb2d98a96e03c587828be26f8ff9f634c02062
Henry Nguyen <belugas2005@gmail.com>
2023-09-19 05:44:48 -0500

    Added showDetails function for pokemon clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonRepository = (function () {
   let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
-    { name: "Charmander", height: 0.6, type: ["Fire"] },
-    { name: "Squirtle", height: 0.5, type: ["Water"] },
+    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] },
+    { name: 'Charmander', height: 0.6, type: ['Fire'] },
+    { name: 'Squirtle', height: 0.5, type: ['Water'] },
   ];
 
   return {
@@ -10,17 +10,33 @@ let pokemonRepository = (function () {
       return pokemonList;
     },
     add: function (pokemon) {
-      if (typeof pokemon === object) {
+      if (typeof pokemon === 'object') {
         pokemonList.push(pokemon);
       } else {
         console.error("Error: Invalid data type. Only objects allowed");
       }
     },
+    addListItem: function (pokemon) {
+      let pokemonList = document.querySelector('.pokemonList');
+      let listItem = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('nameButtons');
+      button.addEventListener('click', function () {
+        pokemonRepository.showDetails(pokemon);
+      });
+
+      listItem.appendChild(button);
+      pokemonList.appendChild(listItem);
+    },
+    showDetails: function (pokemon) {
+      console.log(pokemon);
+    },
   };
 })();
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write(
-    "Name: " + pokemon.name + " | Height: " + pokemon.height + "m<br>"
-  );
+document.addEventListener('DOMContentLoaded', function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
