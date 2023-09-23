e9bdde2a3042e5e66c4586b4a51b704f3d836ac1
Henry Nguyen <belugas2005@gmail.com>
2023-09-23 14:28:19 -0500

    Added fetch polyfill & load- functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,77 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-  return {
-    getAll: function () {
-      return pokemonList;
-    },
-    add: function (pokemon) {
-      if (typeof pokemon === 'object') {
-        pokemonList.push(pokemon);
-      } else {
-        console.error("Error: Invalid data type. Only objects allowed");
-      }
-    },
-    addListItem: function (pokemon) {
-      let pokemonList = document.querySelector('.pokemonList');
-      let listItem = document.createElement('li');
-      let button = document.createElement('button');
-      button.innerText = pokemon.name;
-      button.classList.add('nameButtons');
-      button.addEventListener('click', function () {
-        pokemonRepository.showDetails(pokemon);
-      });
-
-      listItem.appendChild(button);
-      pokemonList.appendChild(listItem);
-    },
-    showDetails: function (pokemon) {
-      console.log(pokemon);
-    },
-  };
-  
-  function loadList() {
-    return fetch(apiUrl).then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      json.results.forEach(function(item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url,
-        };
-        add(pokemon);
-      });
-    }).catch(function(e) {
-      console.error(e);
-    })
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
+    } else {
+      console.error("Error: Invalid data type. Only objects allowed");
+    }
   }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemonList");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("nameButtons");
+    button.addEventListener("click", function () {
+      showDetails(pokemon);
+    });
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
     loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
