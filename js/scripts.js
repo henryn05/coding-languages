1396837d888e0aebd076e654a66f7cc0c0701a7f
Henry Nguyen <belugas2005@gmail.com>
2023-10-04 16:00:20 -0500

    Capitalized names, added two methods for esc modal

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ let pokemonRepository = (function () {
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   function add(pokemon) {
-    if (typeof pokemon === 'object') {
+    if (typeof pokemon === "object") {
       pokemonList.push(pokemon);
     } else {
-      console.error('Error: Invalid data type. Only objects allowed');
+      console.error("Error: Invalid data type. Only objects allowed");
     }
   }
 
@@ -15,12 +15,12 @@ let pokemonRepository = (function () {
   }
 
   function addListItem(pokemon) {
-    let pokemonList = document.querySelector('.pokemonList');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
+    let pokemonList = document.querySelector(".pokemonList");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add('nameButtons');
-    button.addEventListener('click', function () {
+    button.classList.add("nameButtons");
+    button.addEventListener("click", function () {
       showDetails(pokemon);
     });
     listItem.appendChild(button);
@@ -29,13 +29,13 @@ let pokemonRepository = (function () {
   }
 
   function showLoadingMessage() {
-    let loadingMessage = document.getElementById('loadingMessage');
-    loadingMessage.style.display = 'block';
+    let loadingMessage = document.getElementById("loadingMessage");
+    loadingMessage.style.display = "block";
   }
 
   function hideLoadingMessage() {
-    let loadingMessage = document.getElementById('loadingMessage');
-    loadingMessage.style.display = 'none';
+    let loadingMessage = document.getElementById("loadingMessage");
+    loadingMessage.style.display = "none";
   }
 
   function loadDetails(item) {
@@ -46,6 +46,7 @@ let pokemonRepository = (function () {
         return response.json();
       })
       .then(function (details) {
+        console.log(details);
         hideLoadingMessage();
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
@@ -55,26 +56,6 @@ let pokemonRepository = (function () {
         hideLoadingMessage();
         console.error(e);
       });
-  }
-
-  function showDetails(pokemon) {
-    loadDetails(pokemon).then(function (details) {
-      let modal = document.querySelector(".modal");
-      let modalContainer = document.querySelector(".modalContainer");
-      let modalContent = modal.querySelector("p");
-      let overlay = document.querySelector(".overlay");
-
-      modalContent.textContent = `Name: ${details.name}, Height: ${details.height}`;
-      modalContent.innerHTML += `<img src="${details.imageUrl}" alt="${details.name}">`;
-
-      modalContainer.classList.add("is-visible");
-      overlay.classList.add("is-visible");
-
-      document.getElementById("closeModal").addEventListener("click", function () {
-        modalContainer.style.display = "none";
-        overlay.classList.remove("is-visible");
-      });
-    });
   }
 
   function loadList() {
@@ -92,11 +73,85 @@ let pokemonRepository = (function () {
           };
           add(pokemon);
         });
+        // Capitalize the first letter of each Pokémon's name
+        pokemonList.forEach(function (pokemon) {
+          pokemon.name = capitalizeFirstLetter(pokemon.name);
+        });
       })
       .catch(function (e) {
         hideLoadingMessage();
         console.error(e);
       });
+  }
+
+  // A little help from Chat-GPT
+  function showDetails(pokemon) {
+    // Check if the height is already loaded
+    let modalContainer = document.querySelector(".modalContainer");
+    let overlay = document.querySelector(".overlay");
+
+    if (typeof pokemon.height === "undefined") {
+      // If not loaded, load the details first
+      loadDetails(pokemon).then(function () {
+        displayPokemonDetails(pokemon);
+      });
+    } else {
+      // If already loaded, display the details immediately
+      displayPokemonDetails(pokemon);
+    }
+
+    // Remove any existing event listeners from the close button
+    document
+      .getElementById("closeModal")
+      .removeEventListener("click", closeModal);
+
+    // Add a new event listener to close the modal when the "Close" button is clicked
+    document.getElementById("closeModal").addEventListener("click", closeModal);
+
+    // Add event listener to close modal when "Esc" key is pressed
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "Escape") {
+        closeModal();
+      }
+    });
+
+    // Add event listener to close modal when clicked outside of modal
+    overlay.addEventListener("click", closeModal);
+
+    function closeModal() {
+      modalContainer.classList.remove("is-visible");
+      overlay.classList.remove("is-visible");
+
+      document
+        .getElementById("closeModal")
+        .removeEventListener("click", closeModal);
+      document.removeEventListener("keydown", closeModal);
+      overlay.removeEventListener("click", closeModal);
+    }
+  }
+
+  function displayPokemonDetails(pokemon) {
+    let modal = document.querySelector(".modal");
+    let modalContainer = document.querySelector(".modalContainer");
+    let overlay = document.querySelector(".overlay");
+    let modalContent = modal.querySelector("p");
+
+    modalContent.textContent = `Name: ${pokemon.name}, Height: ${pokemon.height}m`;
+    modalContent.innerHTML += `<img src="${pokemon.imageUrl}" alt="${pokemon.name}">`;
+
+    modalContainer.classList.add("is-visible");
+    overlay.classList.add("is-visible");
+
+    document
+      .getElementById("closeModal")
+      .addEventListener("click", function () {
+        modalContainer.style.display = "none";
+        overlay.classList.remove("is-visible");
+      });
+  }
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   return {
